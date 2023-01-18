c68a40c3c6cd77f810931cc8e7030d4edc58f464
khishigtsooj <Khishigtsoojbathuyg@gmail.com>
2023-01-18 15:07:41 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 //Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0 хоёрдугаар тоглогчийг 1 гэж тэмдэглэе
 var activePlayer = 1;
+
+
 //Тоглогчдын цуглуулсан оноог хадгалах хувьсагч 
 var scores = [0,0];
+
+
 // Тоглогчийн ээлжиндээ цуглуулаж байгаа оноог хадгалах хувьсагч 
 var roundScore = 0;
+
+
 // Шооны аль талаараа буусаныг хадгалах хувьсагч хэрэгтэй 1-6 гэсэн утгыг энэ
 //санамсаргүйгээр үүсгэж өгнө
-var dice = Math.floor( Math.random()*6)+1;
 
 
 //Програм эхлэхэд бэлтгэе
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = '0';
+document.getElementById("score-1").textContent = '0';
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
 
-document.querySelector(".dice").style.display = "none";
+document.querySelector('.btn-roll').addEventListener('click',function (){
+    var diceNumber = Math.floor( Math.random()*6) + 1;
+
+   diceDom.style.display = "block";
+   diceDom.src = 'dice-' + diceNumber + ".png";
+ 
+});
+
+
 
